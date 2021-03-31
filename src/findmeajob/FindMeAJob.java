66cf75116d43ab324f8323220e91fa43520b6af5
Cristian Cubillos <ccubillosreyes123@gmail.com>
2021-03-30 19:37:05 -0500

    Commit despreciable

--- src/findmeajob/FindMeAJob.java
+++ src/findmeajob/FindMeAJob.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package findmeajob;
 
 import findmeajob.scrappers.ComputrabajoScrapper;
@@ -16,21 +12,18 @@ import java.util.List;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
+   
 public class FindMeAJob {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) throws IOException{
         
         SupportTools supportTools = new SupportTools();
-        ComputrabajoScrapper cpScrapper = new ComputrabajoScrapper("Ingeniero de sistemas", "bogota dc");
+        ComputrabajoScrapper cpScrapper = new ComputrabajoScrapper("panadero", "bogota dc");
         
         ArrayList<List> offersData = cpScrapper.scrappingAll();
         System.out.println(offersData.size());
         supportTools.printTable(offersData);
         
-        
     }
 }
+    
